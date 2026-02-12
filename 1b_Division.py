b5cb92aa028eeea0918ee093dbe7a0e464358b87
Anusuya <anusuyasenthilvel322@gmail.com>
2026-02-12 21:57:20 +0530

    Added division program

---
+++ 1b_Division.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter a number(numerator):"))
+num2 = int(input("Enter a number(denominator:"))
+if num2 != 0:
+    print("Division of ", num1, "and", num2, ": ", num1 / num2)
+else:
+    print("NaN")
+
