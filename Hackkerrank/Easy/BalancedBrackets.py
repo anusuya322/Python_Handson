e969ef16a41a32ed24ab1370c92445d45c9c3f4d
Anusuya <anusuyasenthilvel322@gmail.com>
2026-02-14 21:34:18 +0530

    List and Tuples

---
+++ Hackkerrank/Easy/BalancedBrackets.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'countResponseTimeRegressions' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY responseTimes as parameter.
+#
+
+def countResponseTimeRegressions(responseTimes):
+    # Write your code here
+    if len(responseTimes)==0:
+        return 0
+    count=0
+    total=responseTimes[0]
+    for i in range(1,len(responseTimes)):
+        avg=total/i
+        if responseTimes[i]>avg:
+            count+=1
+        total+=responseTimes[i]
+    return count
+
+if __name__ == '__main__':
+    responseTimes_count = int(input().strip())
+
+    responseTimes = []
+
+    for _ in range(responseTimes_count):
+        responseTimes_item = int(input().strip())
+        responseTimes.append(responseTimes_item)
+
+    result = countResponseTimeRegressions(responseTimes)
+
+    print(result)
---
+++ Hackkerrank/Easy/SmallestMissingNumber.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'findSmallestMissingPositive' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY orderNumbers as parameter.
+#
+
+def findSmallestMissingPositive(orderNumbers):
+    # Write your code here
+    '''for i in range(len(orderNumbers)):
+        if i not in orderNumbers:
+            return i'''
+    nums=set(orderNumbers)
+    smallest=1
+    while True:
+        if smallest not in nums:
+            return smallest
+        smallest+=1
+
+if __name__ == '__main__':
+    orderNumbers_count = int(input().strip())
+
+    orderNumbers = []
+
+    for _ in range(orderNumbers_count):
+        orderNumbers_item = int(input().strip())
+        orderNumbers.append(orderNumbers_item)
+
+    result = findSmallestMissingPositive(orderNumbers)
+
+    print(result)
